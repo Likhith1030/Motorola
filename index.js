69d1dbb13cc06b4a3c096f812261a7b28bf99675
Likky <95261477+Likhith1030@users.noreply.github.com>
2022-02-13 00:47:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Get the current year for the copyright
+$("#year").text(new Date().getFullYear());
+//scrolling-effects
+$("body").scrollspy({ target: "main-nav" });
+$("#main-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
